62a19bd446fddb23b5bb2bafad40ff9ed0e4a799
knlkshyp <knlkshyp@gmail.com>
2024-03-12 10:50:16 +0530

    Minor chage

--- account/src/main/java/com/bank/account/AccountApplication.java
+++ account/src/main/java/com/bank/account/AccountApplication.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @OpenAPIDefinition(
@@ -30,6 +31,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		)
 )
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@RefreshScope
 @SpringBootApplication
 public class AccountApplication {
 
--- loan/src/main/java/com/bank/loan/LoanApplication.java
+++ loan/src/main/java/com/bank/loan/LoanApplication.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@RefreshScope
 @SpringBootApplication
 @OpenAPIDefinition(
 		info = @Info(
