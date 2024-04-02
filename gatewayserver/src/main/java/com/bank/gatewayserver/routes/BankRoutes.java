d1ac1c171b9052b2c75ca1da2fd0295f22634be7
knlkshyp <knlkshyp@gmail.com>
2024-04-02 16:15:26 +0530

    Add gateway redis rate limiter

--- gatewayserver/src/main/java/com/bank/gatewayserver/routes/BankRoutes.java
+++ gatewayserver/src/main/java/com/bank/gatewayserver/routes/BankRoutes.java
@@ -1,10 +1,13 @@
 package com.bank.gatewayserver.routes;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -12,12 +15,18 @@ import java.time.LocalDateTime;
 @Configuration
 public class BankRoutes {
 
+    public static final String USER = "user";
+    public static final String USER_ANONYMOUS = "anonymous";
+
     @Bean
     public RouteLocator routeLocatorBean(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(path -> path.path("/bank/account/**")
                         .filters(filter -> filter.rewritePath("/bank/account/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver()))
                                 .circuitBreaker(config -> config
                                         .setName("accountCircuitBreaker")
                                         .setFallbackUri("forward:/error"))
@@ -35,5 +44,16 @@ public class BankRoutes {
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://LOAN"))
                 .build();
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst(USER))
+                .defaultIfEmpty(USER_ANONYMOUS);
     }
 }
